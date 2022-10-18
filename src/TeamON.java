81b99a0930344cb8eb7c613ae8f5c7f3b9fb781a
Nijat1 <nganiyev@gmail.com>
2022-10-17 20:49:46 -0500

    Put in Test1

--- src/TeamON.java
+++ src/TeamON.java
@@ -2,6 +2,7 @@ public class TeamON {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test 2");
+        System.out.println("Dont put your husband in a while loop");
 
     }
 }
